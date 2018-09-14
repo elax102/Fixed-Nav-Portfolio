96307716a9020a2df0eaf8adf21ba5438e8a7856
Alex <davis.alex.jr@gmail.com>
2018-09-14 16:12:32 -0400

    Added cards and background change code

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,17 @@
-var helloWorld = function() {
-	console.log('Hello World');
-};
+var waypoint = new Waypoint({
+	element: document.getElementById('about'),
+	handler: function () {
+		console.log('scrolled about');
+		document.body.style.backgroundColor = "#eee";
+	},
+	offset: -200
+})
 
-helloWorld();
+var waypoint = new Waypoint({
+	element: document.getElementById('project'),
+	handler: function () {
+		console.log('scrolled project');
+		document.body.style.backgroundColor = "pink";
+	},
+	offset: 200
+})
